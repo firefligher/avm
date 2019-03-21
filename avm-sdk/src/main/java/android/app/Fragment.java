a2ffaca14f8d6e805201d61b23eb180fc87805b4
Luis Felger <firefligher.tornado@gmail.com>
2019-03-21 20:52:21 +0100

    Fixed an incompatibility

--- avm-sdk/src/main/java/android/app/Fragment.java
+++ avm-sdk/src/main/java/android/app/Fragment.java
@@ -1,7 +1,25 @@
 package android.app;
 
 import android.content.ComponentCallbacks2;
+import android.content.res.Configuration;
 import android.view.View;
+import lombok.extern.java.Log;
 
+@Log
 public class Fragment implements ComponentCallbacks2, View.OnCreateContextMenuListener {
+    @Override
+    public void onTrimMemory(int level) {
+        log.warning("Unimplemented method: android.app.Fragment.onTrimMemory(int)");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        log.warning("Unimplemented method: android.app.Fragment.onConfigurationChanged(" +
+                "android.content.res.Configuration)");
+    }
+
+    @Override
+    public void onLowMemory() {
+        log.warning("Unimplemented method: android.app.Fragment.onLowMemory()");
+    }
 }
